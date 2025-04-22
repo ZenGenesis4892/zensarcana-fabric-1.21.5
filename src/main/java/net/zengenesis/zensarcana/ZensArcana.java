c7bf09fbde0a6ec6d78604f4fb8d236413c5c8e1
Your Name <zengenesis4892@gmail.com>
2025-04-21 21:06:12 -0700

    Initial commit

---
+++ src/main/java/net/zengenesis/zensarcana/ZensArcana.java
@@ -0,0 +1,17 @@
+package net.zengenesis.zensarcana;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ZensArcana implements ModInitializer {
+	public static final String MOD_ID = "zensarcana";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/zengenesis/zensarcana/ZensArcanaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.zengenesis.zensarcana;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ZensArcanaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
