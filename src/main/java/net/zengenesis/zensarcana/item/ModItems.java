30d8225266be17f2ee243dbf7db6a83d78d37150
Your Name <zengenesis4892@gmail.com>
2025-05-21 18:09:49 -0700

    Added wand with no functionality yet

--- src/main/java/net/zengenesis/zensarcana/item/ModItems.java
+++ src/main/java/net/zengenesis/zensarcana/item/ModItems.java
@@ -9,6 +9,8 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.zengenesis.zensarcana.ZensArcana;
+import net.zengenesis.zensarcana.item.custom.WandItem;
+
 //For new items, copy public static final function, add new entry under ItemGroupEvents, add into en_us.json,
 //add json in items, add json in models/item, and add textures
 public class ModItems {
@@ -18,6 +20,9 @@ public class ModItems {
             .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(ZensArcana.MOD_ID,"aurium_shards")))));
     public static final Item AURIUM_PLASTEEL = registerItem("aurium_plasteel", new Item(new Item.Settings()
             .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(ZensArcana.MOD_ID,"aurium_plasteel")))));
+
+    public static final Item WAND = registerItem("wand", new Item(new Item.Settings().maxDamage(32)
+            .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(ZensArcana.MOD_ID,"wand")))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(ZensArcana.MOD_ID, name), item);
---
+++ src/main/java/net/zengenesis/zensarcana/item/custom/WandItem.java
@@ -0,0 +1,50 @@
+package net.zengenesis.zensarcana.item.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.world.World;
+import net.zengenesis.zensarcana.block.ModBlocks;
+
+import java.util.Map;
+
+public class WandItem extends Item {
+    private static  final Map<Block, Block> WAND_MAP =
+            Map.of(
+                    Blocks.STONE, Blocks.STONE_BRICKS,
+                    Blocks.END_STONE, Blocks.END_STONE_BRICKS,
+                    Blocks.OAK_LOG, ModBlocks.AURIUM_PLASTEEL_BLOCK
+            );
+
+    public WandItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+        Block clickedBlock = world.getBlockState(context.getBlockPos()).getBlock();
+
+        if(WAND_MAP.containsKey(clickedBlock)) {
+            if(!world.isClient()) {
+                world.setBlockState(context.getBlockPos(), WAND_MAP.get(clickedBlock).getDefaultState());
+
+                context.getStack().damage(1, ((ServerWorld) world), ((ServerPlayerEntity) context.getPlayer()),
+                        item -> context.getPlayer().sendEquipmentBreakStatus(item, EquipmentSlot.MAINHAND));
+
+                world.playSound(null, context.getBlockPos(), SoundEvents.BLOCK_GRINDSTONE_USE, SoundCategory.BLOCKS);
+
+            }
+
+        }
+
+        return ActionResult.SUCCESS;
+    }
+}
